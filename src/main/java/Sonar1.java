bcf693507813cd27c9eaf4aa389d5dd33268af53
kushagra67414 <500067414@stu.upes.ac.in>
2020-08-25 12:51:03 +0530

    jenkins file

---
+++ src/main/java/Sonar1.java
@@ -0,0 +1,36 @@
+class MultithreadingDemo extends Thread 
+{ 
+    public void run() 
+    { 
+        try
+        { 
+            // Displaying the thread that is running 
+            System.out.println ("Thread " + 
+                  Thread.currentThread().getId() + 
+                  " is running"); 
+  
+        } 
+        catch (Exception e) 
+        { 
+            // Throwing an exception 
+            System.out.println ("Exception is caught"); 
+        } 
+    } 
+} 
+  
+// Main Class 
+public class Sonar1 
+{ 
+    public static void main(String[] args) 
+    { 
+       int s;
+       String av;
+    	int n = 11; // Number of threads 
+        for (int i=0; i<n; i++) 
+        { 
+            MultithreadingDemo object = new MultithreadingDemo(); 
+            object.start(); 
+        } 
+    } 
+} 
+
